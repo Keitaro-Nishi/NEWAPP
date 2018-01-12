f882504e17a9dc6536869d1b431a7268233f98a9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-12 10:18:26 +0900

    s

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,130 +1,101 @@
-/*
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Set;
+import java.io.Serializable;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Table;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
+/**
+ * The persistent class for the chatbot database table.
+ * aaa
+ */
 @Entity
-@Table(name="userdata")
-public class User {
-
+@Table(name="custinfo")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-
-    @GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "no",nullable = false)
-    private int no;
-
-    @Id
-    @Column(name = "custid",nullable = false, unique = true)
-    private String username;
-
-    @Column(name = "custname",nullable = false, unique = true)
-    private String custname;
-
-    @Column(name = "orgname",nullable = false, unique = true)
-    private String orgname;
-
-    @Column(name = "password",nullable = false)
-    private String password;
-
-    @Column(name = "role",nullable = false)
-    private String role;
-
-    @Column(name = "reserve",nullable = false)
-    private String reserve;
-
-
-    // JPA requirement
-    protected User() {}
-
-    public User(int no,String custid, String custname, String orgname, String password ,String role,String reserve) {
-    	this.no = no;
-    	this.username = custid;
-    	this.custname = custname;
-    	this.orgname = orgname;
-        this.password = password;
-        this.role = role;
-        this.reserve = reserve;
-    }
-
-
-    //@Override
-    public int getNo() {
-        return no;
-    }
-    public void setNo(int no) {
-        this.no = no;
-    }
-
-    //@Override
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    //@Override
-    public String getCustname() {
-        return custname;
-    }
-    public void setCustname(String custname) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer no;
+	@Column
+	private String custid;
+	@Column
+	private String custname;
+	@Column
+	private String orgname;
+	@Column
+	private String password;
+	@Column
+	private String effect;
+	@Column
+	private String role;
+	
+	
+	public User() {
+	}
+	
+	public User(Integer no,String custid,String custname,String orgname,String password,String effect,String role) {
+		this.no = no; 
+		this.custid = custid;
         this.custname = custname;
-    }
-
-    //@Override
-    public String getOrgname() {
-        return orgname;
-    }
-    public void setOrgname(String orgname) {
         this.orgname = orgname;
-    }
-
-    //@Override
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    //@Override
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
+        this.effect = effect;
         this.role = role;
     }
 
-    //@Override
-    public String getReserve() {
-        return role;
-    }
-    public void setReserve(String reserve) {
-        this.reserve = reserve;
-    }
+	public Integer getNo() {
+		return this.no;
+	}
+
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+	public String getCustid() {
+		return this.custid;
+	}
+
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+
+	public String getCustname() {
+		return this.custname;
+	}
+
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+	
+	public String getOrgname() {
+		return this.orgname;
+	}
+
+	public void setOrgname(String orgname) {
+		this.orgname = orgname;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEffect() {
+		return this.effect;
+	}
+
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
 }
-*/
--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -1,21 +1,24 @@
-/*
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.User;
+import com.example.UserRepository;
 
 @Service
-public class UserService implements UserDetailsService {
-
+public class UserService {
+	
 	@Autowired
-	private UserRepository repository;
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
+	private UserRepository userRepository;	
+	
+	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	
+	@Transactional
+    public void registerCustomer(Integer no,String custid,String custname,String orgname,String password,String effect,String role) {
+		User user = new User(no,custid,custname,orgname,passwordEncoder.encode(password),effect,role);
+		userRepository.save(user);
+    }
 }
-*/
